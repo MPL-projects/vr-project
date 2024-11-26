d6c08edfd1ab43cf1351468b024b0cca057ed96f
Louis LE LAY <le.lay.louis@gmail.com>
2024-11-26 14:10:20 -0500

    adding logic to detecct that the ball is in the trashcan

--- Assets/score.cs
+++ Assets/score.cs
@@ -6,6 +6,8 @@ public class Score : MonoBehaviour
 {
     private bool isScored;
     private GameObject corbeille;
+    private GameObject cylinder;
+    private GameObject ball;
 
     // Start is called before the first frame update
     void Start()
@@ -13,19 +15,28 @@ public class Score : MonoBehaviour
         isScored = false;
 
         corbeille = GameObject.Find("Corbeille");
-        if (corbeille == null)
-        {
-            Debug.LogError("Corbeille non trouvée dans la scène !");
-        }
+        cylinder = GameObject.Find("Cylinder");
+        ball = GameObject.Find("[BuildingBlock] Grabbable sphere");
+
+        if (corbeille == null){ Debug.LogError("Corbeille non trouvée dans la scène"); }
+        if (cylinder == null){ Debug.LogError("Cylindre non trouvée dans la scène"); }
+        if (ball == null){ Debug.LogError("Balle non trouvée dans la scène"); }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isScored /*condition*/)
+        // Vérifie si la distance entre la balle et le cylindre est suffisamment petite pour indiquer un contact
+        float distance = Vector3.Distance(ball.transform.position, cylinder.transform.position);
+        isScored = distance < 0.5f;
+
+        if (isScored)
         {
-            isScored = false;
-            corbeille.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+            // Corbeille prends une couleur au hasard
+            // corbeille.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+            
+            // Corbeille verte
+            corbeille.GetComponent<MeshRenderer>().material.color = new Color(0f, 1f, 0f, 1f);
         }
     }
 }
